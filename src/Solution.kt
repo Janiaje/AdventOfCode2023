9e7ee808002ca09ec288b08e308939f2560c8c67
Janos Tihanyi <tihanyi.janos@gmail.com>
2023-12-01 19:53:28 +0100

    Day 1

--- src/Solution.kt
+++
@@ -1,9 +0,0 @@
-import java.io.File
-
-abstract class Solution {
-
-    val inputFile = File("src/${this::class.simpleName!!.lowercase()}/input.txt")
-
-    abstract fun compute()
-
-}
--- src/day1/Day1.kt
+++ src/day1/Day1.kt
@@ -1,14 +1,52 @@
 package day1
 
-import Solution
+import java.io.File
 
-object Day1 : Solution() {
+object Day1 {
 
-    override fun compute() {
-        println(inputFile.readLines())
+    private val inputFile = File("src/day1/input.txt")
+
+    private val firstDigitRegex = "^\\D*?(?<first>\\d)".toRegex()
+    private val lastDigitRegex = "(?<last>\\d)\\D*$".toRegex()
+
+    private val firstDigitRegexPart2 = "^\\D*?(?<first>\\d|one|two|three|four|five|six|seven|eight|nine)".toRegex()
+    private val lastDigitRegexPart2 = "^\\D*?(?<last>\\d|${"one|two|three|four|five|six|seven|eight|nine".reversed()})"
+        .toRegex()
+
+    fun compute() {
+        println("The part one results are ${part1()}")
+        println("The part two results are ${part2()}")
     }
 
+    private fun part1() = inputFile
+        .readLines()
+        .sumOf {
+            val firstDigit = firstDigitRegex.find(it)!!.groups["first"]!!.value
+            val lastDigit = lastDigitRegex.find(it)!!.groups["last"]!!.value
+            "$firstDigit$lastDigit".toInt()
+        }
 
+    private fun part2() = inputFile
+        .readLines()
+        .sumOf {
+            val firstDigit = firstDigitRegexPart2.find(it)!!.groups["first"]!!.value.parseToInt()
+            val lastDigit = lastDigitRegexPart2.find(it.reversed())!!.groups["last"]!!.value.reversed().parseToInt()
+            "$firstDigit$lastDigit".toInt()
+        }
+
+    private fun String.parseToInt() = when (this) {
+        "zero" -> 0
+        "one" -> 1
+        "two" -> 2
+        "three" -> 3
+        "four" -> 4
+        "five" -> 5
+        "six" -> 6
+        "seven" -> 7
+        "eight" -> 8
+        "nine" -> 9
+        else -> toInt()
+    }
 }
 
 fun main() {
