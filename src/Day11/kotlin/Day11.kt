c8bce6817f8c85babff637cc0889d37ad707845c
Janos Tihanyi <tihanyi.janos@gmail.com>
2023-12-17 19:29:52 +0100

    Day11 task

---
+++ src/Day11/kotlin/Day11.kt
@@ -0,0 +1,25 @@
+import java.io.File
+
+fun main() {
+    Day11.compute()
+}
+
+object Day11 {
+
+    private val inputFile = File("src/Day11/kotlin/input.txt")
+
+    // private val text = inputFile.readLines()
+    private val text = listOf(
+        "",
+    )
+
+    fun compute() {
+        println("Part 1 => ${part1()}")
+        // println("Part 2 => ${part2()}")
+    }
+
+    private fun part1() = text
+
+    private fun part2() = text
+
+}
