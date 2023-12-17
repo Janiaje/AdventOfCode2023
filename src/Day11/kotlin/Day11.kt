22ac29167ad490571f4414cdaebf827651d89bf3
Janos Tihanyi <tihanyi.janos@gmail.com>
2023-12-17 20:52:31 +0100

    Day11 solutions

--- src/Day11/kotlin/Day11.kt
+++ src/Day11/kotlin/Day11.kt
@@ -23,27 +23,73 @@ object Day11 {
     //     "#...#.....",
     // )
 
-    private const val EMPTY_SPACE_CHAR = '.'
     private const val GALAXY_CHAR = '#'
 
     fun compute() {
         println("Part 1 => ${part1()}")
-        // println("Part 2 => ${part2()}")
+        println("Part 2 => ${part2()}")
     }
 
     data class Galaxy(val x: Int, val y: Int) {
 
         companion object {
-            private var id = 1
+            private val emptyRowIndexes = text
+                .mapIndexedNotNull { index, row ->
+                    if (!row.contains(GALAXY_CHAR)) {
+                        index
+                    } else {
+                        null
+                    }
+                }
+
+            private val emptyColumnIndexes = text
+                .map { it.toList() }
+                .transpose()
+                .mapIndexedNotNull { index, row ->
+                    if (!row.contains(GALAXY_CHAR)) {
+                        index
+                    } else {
+                        null
+                    }
+                }
         }
 
-        val id = Galaxy.id++
+        fun distanceTo(other: Galaxy, expansionRatio: Long): Long {
 
-        fun distanceTo(other: Galaxy): Int = abs(x - other.x) + abs(y - other.y)
+            val originalDistance = abs(x - other.x) + abs(y - other.y)
+
+            val xRange = x.properRange(other.x)
+            val yRange = y.properRange(other.y)
+
+            val expansionDistance = listOf(
+                emptyColumnIndexes.filter { xRange.contains(it) },
+                emptyRowIndexes.filter { yRange.contains(it) },
+            )
+                .flatten()
+                .count()
+                .let { it * (expansionRatio - 1) }
+
+            return originalDistance + expansionDistance
+        }
 
     }
 
-    private fun part1(): Int = expandMap(text)
+    private fun Int.properRange(other: Int) = if (this <= other) this..other else other..this
+
+    private fun List<List<Char>>.transpose(): List<List<Char>> {
+        val cols = this[0].size
+        val rows = this.size
+        return List(cols) { j ->
+            List(rows) { i ->
+                this[i][j]
+            }
+        }
+    }
+
+    private fun part1() = computeDistancesSum(2L)
+
+    private fun computeDistancesSum(expansionRatio: Long) = text
+        .map { it.toList() }
         .flatMapIndexed { y: Int, row: List<Char> ->
             row.mapIndexedNotNull { x, cell ->
                 if (cell == GALAXY_CHAR) {
@@ -54,40 +100,13 @@ object Day11 {
             }
         }
         .let { generateRoutes(it) }
-        .sumOf { it.first.distanceTo(it.second) }
+        .sumOf { it.first.distanceTo(it.second, expansionRatio) }
 
     private fun generateRoutes(galaxies: List<Galaxy>): List<Pair<Galaxy, Galaxy>> =
         galaxies.flatMapIndexed { index, first ->
             galaxies.drop(index + 1).map { second -> Pair(first, second) }
         }
 
-    private fun expandMap(text: List<String>): List<List<Char>> = text
-        .map { it.toList() }
-        .let { expandRows(it) }
-        .let { transpose(it) }
-        .let { expandRows(it) }
-        .let { transpose(it) }
-
-    private fun expandRows(text: List<List<Char>>): List<List<Char>> = text.flatMap {
-
-        return@flatMap if (it.contains('#')) {
-            listOf(it)
-        } else {
-            listOf(it, it)
-        }
-
-    }
-
-    private inline fun <reified T> transpose(matrix: List<List<T>>): List<List<T>> {
-        val cols = matrix[0].size
-        val rows = matrix.size
-        return List(cols) { j ->
-            List(rows) { i ->
-                matrix[i][j]
-            }
-        }
-    }
-
-    private fun part2() = text
+    private fun part2() = computeDistancesSum(1_000_000L)
 
 }
